8cf0fc8fce5b907b4011888590d98c361969bfb8
Stacey Reiman <mspanish@zoho.com>
2013-08-22 18:39:48 -0800

    working computed fields in table generated purely from the model, not the template :)

--- public/app-marionette.js
+++ public/app-marionette.js
@@ -187,29 +187,37 @@ cards.fetch({ url: 'card.json' })
 function fetchItems() {
 	console.log('our old table model is ' + JSON.stringify(table))
    $.get('items.json', function(data) {
-var fieldsArray =  [];
-var fieldsObject = {
-	'fields' : fieldsArray
-};
+// now, if we want computed values, we need to know which rows we want computed, and instead of running a simple 'each' on them, we can compute the rows we want, create a new object with all "field" entries, and submit that instead. The rows and fields in the Table model must still be the SAME number and match up!!
 
+/* this is without any computed values 
 $(data).each(function() {
-var personArray;
-
-fieldsArray = [];
-// var field1 = {'field': $(this)[0].firstname + ' ' + $this)[0].lastname}
-// var field2 = {'field': $(this)[0].email}
-// var field3 = {'field': $(this)[0].description}
-fieldsArray.push({'field': 'yo'})
-fieldsArray.push({'field': 'yo2'})		
-fieldsArray.push({'field': 'yo3'})
-
-table.add('items.fields',[{'field':'yo'}]);
+console.log('our array data is ' + $(this)[0] )
+table.add('items',$(this)[0]);
 });
+*/
+
+/* this is WITH computed values */
+$(data).each(function() {
+console.log('our array data is ' + $(this)[0] )
+var fullName = $(this)[0].fields[0].field + ' '+ $(this)[0].fields[1].field;
+var email = $(this)[0].fields[2].field ;
+var description = $(this)[0].fields[3].field ;
+
+var computedObject = {fields:[
+{"field": fullName},
+{"field": email},
+{"field": description}
+	]
+}
+console.log('computedObject is ' + JSON.stringify(computedObject))
+table.add('items', computedObject);
+});
+
  console.log('updated table model ' + JSON.stringify(table))
     });
 }
 
-//fetchItems();
+fetchItems();
 
 // cFinally, we render views in our app
 
